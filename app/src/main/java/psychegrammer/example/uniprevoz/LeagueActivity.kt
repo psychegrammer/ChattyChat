7105d94705481e62c7623022ac353b4f4534b0d2
stafamucalju <ximuboo@gmail.com>
2019-07-11 22:47:18 +0200

    lesson 2

---
+++ app/src/main/java/psychegrammer/example/uniprevoz/LeagueActivity.kt
@@ -0,0 +1,12 @@
+package psychegrammer.example.uniprevoz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
