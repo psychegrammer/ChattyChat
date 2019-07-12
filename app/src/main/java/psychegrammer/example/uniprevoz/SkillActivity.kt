e499eba6bcee5c33259255b9b1fa233c6baa36a1
stafamucalju <ximuboo@gmail.com>
2019-07-12 13:50:24 +0200

    lesson 6

---
+++ app/src/main/java/psychegrammer/example/uniprevoz/SkillActivity.kt
@@ -0,0 +1,12 @@
+package psychegrammer.example.uniprevoz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
